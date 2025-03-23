8bef9d9a22e482e425f4624a46d8e304579e113b
Eroscodex <karlalondra2003@gmail.com>
2025-03-23 17:19:21 +0800

    first commit

--- delete_employee.php
+++ delete_employee.php
@@ -8,14 +8,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $delete = $conn->query($delete_sql);
 
     if ($delete) {
-    
-        $reset_sql1 = "SET @count = 0;";
-        $reset_sql2 = "UPDATE employee SET id = (@count := @count + 1) ORDER BY id ASC;"; 
-        $reset_sql3 = "ALTER TABLE employee AUTO_INCREMENT = 1;";
+ 
+        $conn->query("SET @count = 0;");
+        $conn->query("UPDATE employee SET id = (@count := @count + 1) ORDER BY id ASC;");
 
-        $conn->query($reset_sql1);
-        $conn->query($reset_sql2);
-        $conn->query($reset_sql3);
+       
+        $conn->query("ALTER TABLE employee AUTO_INCREMENT = 1;");
 
         echo "<script>alert('Employee Deleted!'); location.href='index.php';</script>";
     } else {
